cc43d4479735f68e486bdd8897d93b103545af05
Dan Zimmerman <Daniel.zimmerman@mac.com>
2011-01-28 17:29:17 -0600

    i think

--- DeepEnd.m
+++ DeepEnd.m
@@ -32,10 +32,14 @@ static void StartMotion()
 				contentsRect.origin.x = cropLeft + attitude.roll * rollFactor;
 				contentsRect.origin.y = cropLeft + attitude.pitch * pitchFactor;
 <<<<<<< HEAD
+<<<<<<< HEAD
 				CALayer *layer = [[CHSharedInstance(SBUIController) wallpaperView] layer];
 				layer.contentsRect = contentsRect;
 				CGSize size = layer.bounds.size;
 				layer.sublayerTransform = CATransform3DTranslate(scaleTransform, (contentsRect.origin.x - cropLeft) * size.width, (contentsRect.origin.y - cropLeft) * size.height, 0);
+=======
+				[[CHSharedInstance(SBUIController) wallpaperView] layer].contentsRect = contentsRect;
+>>>>>>> parent of 31f7627... Added support for Parallax
 =======
 				[[CHSharedInstance(SBUIController) wallpaperView] layer].contentsRect = contentsRect;
 >>>>>>> parent of 31f7627... Added support for Parallax
