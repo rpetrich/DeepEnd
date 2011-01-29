f70040bc28004b53ebc6ba26a6d8a387fe212b99
Dan Zimmerman <Daniel.zimmerman@mac.com>
2011-01-28 20:32:51 -0600

    Fixed my commits somewhat so they can be pulled. Started to add animation (doesnt work as of now, but is in place to work), added yaw when laying down and roll when down, made it reset when it stops, and as it stop now when an alert is activated as well as when the switcher is activated

--- DeepEnd.m
+++ DeepEnd.m
@@ -8,13 +8,15 @@ CHDeclareClass(SBUIController);
 CHDeclareClass(SBWallpaperView);
 CHDeclareClass(SBAwayController);
 CHDeclareClass(SBAppSwitcherController);
+CHDeclareClass(SBAlertItemsController);
 
 static CMMotionManager *motionManager;
 static double crop;
 static double cropLeft;
 static double rollFactor;
 static double pitchFactor;
-static CGRect originalContentsRect = nil;
+static BOOL animate;
+static CGRect originalContentsRect;
 static CATransform3D scaleTransform;
 
 static void StartMotion()
@@ -23,25 +25,30 @@ static void StartMotion()
 		motionManager = [[CMMotionManager alloc] init];
 		motionManager.deviceMotionUpdateInterval = 1.0 / 30.0;
 	}
+	
+	if (animate)
+		[CATransaction begin];
 	if (!motionManager.deviceMotionActive) {
 		[motionManager startDeviceMotionUpdatesToQueue:[NSOperationQueue mainQueue] withHandler:^(CMDeviceMotion *motion, NSError *error)
-		{
-			if (motion) {
-				CGRect contentsRect;
-				contentsRect.size.width = crop;
-				contentsRect.size.height = crop;
-				CMAttitude *attitude = motion.attitude;
-				contentsRect.origin.x = cropLeft + attitude.roll * rollFactor;
-				contentsRect.origin.y = cropLeft + attitude.pitch * pitchFactor;
-				CALayer *layer = [[CHSharedInstance(SBUIController) wallpaperView] layer];
-				layer.contentsRect = contentsRect;
-				CGSize size = layer.bounds.size;
-				layer.sublayerTransform = CATransform3DTranslate(scaleTransform, (cropLeft - contentsRect.origin.x) * size.width, (cropLeft - contentsRect.origin.y) * size.height, 0);
-				if (!layer.masksToBounds)
-					layer.masksToBounds = YES;
-			}
-		}];
+		 {
+			 if (motion) {
+				 CGRect contentsRect;
+				 contentsRect.size.width = crop;
+				 contentsRect.size.height = crop;
+				 CMAttitude *attitude = motion.attitude;
+				 contentsRect.origin.x = cropLeft + (((M_PI - abs(attitude.roll))/M_PI)*attitude.roll + ((M_PI - abs(attitude.yaw))/(M_PI/2))*attitude.yaw) * rollFactor;
+				 contentsRect.origin.y = cropLeft + attitude.pitch * pitchFactor;
+				 CALayer *layer = [[CHSharedInstance(SBUIController) wallpaperView] layer];
+				 layer.contentsRect = contentsRect;
+				 CGSize size = layer.bounds.size;
+				 layer.sublayerTransform = CATransform3DTranslate(scaleTransform, (cropLeft - contentsRect.origin.x) * size.width, (cropLeft - contentsRect.origin.y) * size.height, 0);
+				 if (!layer.masksToBounds)
+					 layer.masksToBounds = YES;
+			 }
+		 }];
 	}
+	if (animate)
+		[CATransaction commit];
 }
 
 static void StopMotion()
@@ -50,20 +57,32 @@ static void StopMotion()
 		[motionManager stopDeviceMotionUpdates];
 }
 
+static void ResetAndStop()
+{
+	StopMotion();
+	if (animate)
+		[CATransaction begin];
+	CALayer *layer = [[CHSharedInstance(SBUIController) wallpaperView] layer];
+	layer.contentsRect = originalContentsRect;
+	layer.sublayerTransform = CATransform3DIdentity;
+	if (animate)
+		[CATransaction commit];
+}	
+
 CHOptimizedMethod(0, super, void, SBWallpaperView, didMoveToWindow)
 {
 	if (self == [CHSharedInstance(SBUIController) wallpaperView]) {
 		if (self.window)
 			StartMotion();
 		else
-			StopMotion();
+			ResetAndStop();
 	}
 	CHSuper(0, SBWallpaperView, didMoveToWindow);
 }
 
 CHOptimizedMethod(0, self, void, SBAwayController, activate)
 {
-	StopMotion();
+	ResetAndStop();
 	CHSuper(0, SBAwayController, activate);
 }
 
@@ -71,18 +90,38 @@ CHOptimizedMethod(0, self, void, SBAwayController, deactivate)
 {
 	CHSuper(0, SBAwayController, deactivate);
 	if ([[CHSharedInstance(SBUIController) wallpaperView] window]) {
-		if (!originalContentsRect)
-			originalContentsRect = [[CHSharedInstance(SBUIController) wallpaperView] layer].contentsRect;
 		StartMotion();
 	}
 }
 
+CHOptimizedMethod(0, self, void, SBUIController, finishLaunching)
+{
+	CHSuper(0, SBUIController, finishLaunching);
+	originalContentsRect = [[CHSharedInstance(SBUIController) wallpaperView] layer].contentsRect;
+}
+
 CHOptimizedMethod(0, self, void, SBAppSwitcherController, viewWillAppear)
 {
-	StopMotion();
-	[[CHSharedInstance(SBUIController) wallpaperView] layer].contentsRect = originalContentsRect;
-	
+	ResetAndStop();
 	CHSuper(0,SBAppSwitcherController, viewWillAppear);
+}
+
+CHOptimizedMethod(0, self, void, SBAppSwitcherController, viewDidDisappear)
+{
+	StartMotion();
+	CHSuper(0, SBAppSwitcherController, viewDidDisappear);
+}	
+
+CHOptimizedMethod(1, self, void, SBAlertItemsController, activateAlertItem, id, item)
+{
+	ResetAndStop();
+	CHSuper(1, SBAlertItemsController, activateAlertItem, item);
+}
+
+CHOptimizedMethod(1, self, void, SBAlertItemsController, deactivateAlertItem, id, item)
+{
+	StartMotion();
+	CHSuper(1, SBAlertItemsController, deactivateAlertItem, item);
 }
 
 static void LoadSettings()
@@ -97,11 +136,13 @@ static void LoadSettings()
 	rollFactor = (temp ? [temp doubleValue] : 1.0) * cropLeft * (1.0 / M_PI);
 	temp = [dict objectForKey:@"DEPitchFactor"];
 	pitchFactor = (temp ? [temp doubleValue] : 1.0) * cropLeft * (1.0 / M_PI);
+	animate = [dict objectForKey:@"DEAnimate"] ? [[dict objectForKey:@"DEAnimate"] boolValue] : YES;
 	[dict release];
 }
 
 CHConstructor {
 	CHLoadLateClass(SBUIController);
+	CHHook(0, SBUIController, finishLaunching);
 	CHLoadLateClass(SBWallpaperView);
 	CHHook(0, SBWallpaperView, didMoveToWindow);
 	CHLoadLateClass(SBAwayController);
@@ -109,6 +150,10 @@ CHConstructor {
 	CHHook(0, SBAwayController, deactivate);
 	CHLoadLateClass(SBAppSwitcherController);
 	CHHook(0, SBAppSwitcherController, viewWillAppear);
+	CHHook(0, SBAppSwitcherController, viewDidDisappear);
+	CHLoadLateClass(SBAlertItemsController);
+	CHHook(1, SBAlertItemsController, activateAlertItem);
+	CHHook(1, SBAlertItemsController, deactivateAlertItem);
 	CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, (void *)LoadSettings, CFSTR("com.rpetrich.deepend.settingschanged"), NULL, CFNotificationSuspensionBehaviorCoalesce);
 	LoadSettings();
 }
